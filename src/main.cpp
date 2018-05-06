a34f0da4bab853fdfe4425fb82462daf7e1d5e9a
Markus Leballeux <markus.leballeux@gmail.com>
2018-05-06 11:56:33 -0500

    Updated command line arguments to make it easier to use. (IPs and ports can now be provided together)

--- src/main.cpp
+++ src/main.cpp
@@ -3,78 +3,97 @@
 #include <string>
 #include "log.h"
 
+static uint16_t getPort(const std::string& port_arg) {
+    const int port_int = std::stoi(port_arg); //TODO: handle exception
+    if (port_int < 0 || port_int > UINT16_MAX) {
+        DEATH("Invalid port %d!", port_int);
+    }
+    return static_cast<uint16_t>(port_int);
+}
+
+static void getIpPort(const std::string& ip_port_arg, std::string* ip_out, uint16_t* port_out) {
+    size_t pos = ip_port_arg.find(':');
+    
+    //Case where no port is provided
+    if (pos == std::string::npos) {
+        *ip_out = ip_port_arg;
+        return;
+    }
+    if (pos == ip_port_arg.size() - 1) {
+        DEATH("Invalid ip-port argument '%s'.", ip_port_arg.c_str());
+    }
+
+    //Case where ip and port are provided
+    *ip_out = ip_port_arg.substr(0,pos);
+    *port_out = getPort(ip_port_arg.substr(pos + 1)); 
+} 
+
 //Parses the command line arguments to populate the provided DatagramTunneler::Config
 static void getCommandLineConfig(int argc, char* argv[], DatagramTunneler::Config* const cfg) {
-    static struct option long_options[] = {  //TODO: static?
+    static option long_options[] = { //TODO: review the use of static here
         {"server",   no_argument,       0, 's'},
         {"client",   no_argument,       0, 'c'},
         {"udpiface", required_argument, 0, 'i'},
         {"tcpiface", required_argument, 0, 'j'},
-        {"udpdst",   required_argument, 0, 'u'},
-        {"udpport",  required_argument, 0, 'p'},
-        {"tcpip",    required_argument, 0, 't'},
-        {"tcpport",  required_argument, 0, 'l'}
+        {"udpgroup", required_argument, 0, 'u'},
+        {"tcpsrv",   required_argument, 0, 't'}
     };
 
     int c = 0;
+    bool side_selected = false;
     while (c >= 0) {
         int opt_index = 0;
-        if ((c = getopt_long (argc, argv, "sci:j:u:p:t:l:", long_options, &opt_index)) < 0) {
+        if ((c = getopt_long (argc, argv, "sci:j:u:t:", long_options, &opt_index)) < 0) {
             break;//end of options;
         }    
         switch (c)
         {
         case 's': {
-            INFO("Mode:                 server");
+            INFO("Mode:                        server");
             cfg->is_client_ = false;
+            side_selected = true;
             break;
         }
         case 'c': {
-            INFO("Mode:                 client");
+            INFO("Mode:                        client");
             cfg->is_client_ = true;
+            side_selected = true;
             break;
         }
         case 'i': {
-            INFO("UDP interface:        %s", optarg) ;
+            INFO("UDP interface:               %s", optarg) ;
             cfg->udp_iface_ip_ = optarg;
             break;
         }
         case 'j': {
-            INFO("TCP interface:        %s", optarg);
+            INFO("TCP interface:               %s", optarg);
             WARN("TCP interface selection is not supported yet"); //TODO: support interface seleciton
             cfg->tcp_iface_ip_ = optarg;
             break;
         }
         case 'u': {
-            INFO("UDP destination IP:   %s", optarg);
-            cfg->udp_dst_ip_ = optarg;
-            break;
-        }
-        case 'p': {
-            INFO("UDP destination port: %s", optarg);
-            const int port_int = std::stoi(optarg); //TODO: handle exception
-            if (port_int < 0 || port_int > UINT16_MAX) {
-                DEATH("Invalid port %d!", port_int);
-            }
-            cfg->udp_dst_port_ = static_cast<uint16_t>(port_int);
+            INFO("UDP destination IP and port: %s", optarg);
+            std::string ip_port_arg(optarg);
+            getIpPort(ip_port_arg, &cfg->udp_dst_ip_, &cfg->udp_dst_port_);
             break;
         }
         case 't': {
-            INFO("TCP server IP:        %s", optarg);
-            cfg->tcp_srv_ip_ = optarg;
-            break;
-        }
-        case 'l': {
-            INFO("TCP server port:      %s", optarg);
-            const int port_int = std::stoi(optarg); //TODO: handle exception
-            if (port_int < 0 || port_int > UINT16_MAX) {
-                DEATH("Invalid port %d!", port_int);
+            assert(side_selected);
+            std::string ip_port_arg(optarg);
+            if (cfg->is_client_) {
+                INFO("TCP server IP and port:      %s", optarg);
+                getIpPort(ip_port_arg, &cfg->tcp_srv_ip_, &cfg->tcp_srv_port_);
+            } else  {
+                INFO("TCP server listen port:      %s", optarg);
+                cfg->tcp_srv_port_ = getPort(ip_port_arg);
             }
-            cfg->tcp_srv_port_ = static_cast<uint16_t>(port_int);
             break;
         }
         default:
-            DEATH("Unexpected option %d", c);
+            DEATH("Invalid option %d", c);
+        }
+        if (!side_selected) {
+            DEATH("Firt argument needs to be selection of server or client side! (--server or --client)");
         }
     }
 
