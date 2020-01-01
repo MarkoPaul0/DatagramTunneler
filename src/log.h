8155a08d1d3b23ed8312b00d060505060cda3cf2
MarkoPaul0 <markus.leballeux@gmail.com>
2020-01-01 16:29:41 -0600

    Delete log.h

--- src/log.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdio>
-
-#define INFO(_format_,...)  do { printf("INFO      "); printf(_format_,##__VA_ARGS__); printf("\n");          } while (0);
-#define WARN(_format_,...)  do { printf("WARNING   "); printf(_format_,##__VA_ARGS__); printf("\n");          } while (0);
-#define ERROR(_format_,...) do { printf("ERROR     "); printf(_format_,##__VA_ARGS__); printf("\n");          } while (0);
-#define DEATH(_format_,...) do { printf("DEATH     "); printf(_format_,##__VA_ARGS__); printf("\n"); exit(1); } while (0);
